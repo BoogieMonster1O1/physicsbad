3a86b26b3dcb08bd4561d72a6e5050956526da13
SD <harrypotter.starwars.shrish@gmail.com>
2022-06-22 18:06:59 +0530

    Finish main ui

--- src/routes/Main.svelte
+++ src/routes/Main.svelte
@@ -1,16 +1,24 @@
 <div class="flex flex-col font-epicfont justify-center text-center pt-4">
     <div class="text-5xl"><h1>Readings Generator</h1></div>
-    <div class="text-2xl"><h1>For Focal Length Calculation. Be smart, not hardworking.</h1></div>
+    <div class="text-2xl"><h1>Be smart, not hardworking.</h1></div>
+    <br><br>
+    <div class="flex flex-col justify-center">
+        <div><input class="text-2xl border-2 m-4 w-[80%]" type="number" id="desiredFocalLength" name="desiredFocalLength" placeholder="Desired Focal Length (cm)"></div>
+        <div class="text-xl"><input class="w-[80%] text-2xl border-2 m-2" type="number" id="numberOfReadings" name="numberOfReadings" placeholder="Number of Readings"></div>
+        <div class="m-4 text-2xl"><button type="button" class="bg-red-500 px-5 text-white rounded">Generate</button></div>
+    </div>
+    <br><br>
+    <!--TODO-->
     <br><br>
     <div class="text-3xl lg:pl-40 lg:pr-40 lg:text-left">
         What the heck is this?
     </div>
     <div class="text-xl lg:pl-40 lg:pr-40 lg:text-left">
         It's something that extrapolates results.
-        Input your desired focal length and instantly get a set of randomized readings that give a concordant reading of your desired focal length.
-        If you messed up this experiment this'll help you.
+        Input your desired focal length and number of readings required, press the big red button, and instantly get a set of randomized readings that give a concordant reading of your desired focal length.
+        If you messed up this experiment this may come of use to you.
         <br>
-        Github: <a href="https://github.com/BoogieMonster1O1/physicsbad" target="_blank" class="text-blue-600">https://github.com/BoogieMonster1O1/physicsbad</a>
+        <div class="text-s">Source Code: <a href="https://github.com/BoogieMonster1O1/physicsbad" target="_blank" class="text-blue-600">Github</a></div>
     </div>
     <br>
     <div class="text-xs">
