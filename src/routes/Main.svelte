fd0a4014298603c0e16369eb4e87ced3f696b55e
SD <harrypotter.starwars.shrish@gmail.com>
2022-06-22 17:13:26 +0530

    Initial commit

---
+++ src/routes/Main.svelte
@@ -0,0 +1,19 @@
+<div class="flex flex-col font-epicfont justify-center text-center pt-4">
+    <div class="text-5xl"><h1>Readings Generator</h1></div>
+    <div class="text-2xl"><h1>For Focal Length Calculation. Be smart, not hardworking.</h1></div>
+    <br><br>
+    <div class="text-3xl lg:pl-40 lg:pr-40 lg:text-left">
+        What the heck is this?
+    </div>
+    <div class="text-xl lg:pl-40 lg:pr-40 lg:text-left">
+        It's something that extrapolates results.
+        Input your desired focal length and instantly get a set of randomized readings that give a concordant reading of your desired focal length.
+        If you messed up this experiment this'll help you.
+        <br>
+        Github: <a href="https://github.com/BoogieMonster1O1/physicsbad" target="_blank" class="text-blue-600">https://github.com/BoogieMonster1O1/physicsbad</a>
+    </div>
+    <br>
+    <div class="text-xs">
+        Copyright © Shrish Deshpande. MIT Licensed.
+    </div>
+</div>
