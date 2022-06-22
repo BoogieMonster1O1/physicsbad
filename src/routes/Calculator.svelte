3460c3aca3bacde9c6fd26d354cc7d52f4ff1ed3
SD <harrypotter.starwars.shrish@gmail.com>
2022-06-22 22:05:48 +0530

    it works

---
+++ src/routes/Calculator.svelte
@@ -0,0 +1,66 @@
+<script>
+    import {generateReadings, truncateTwo, chadRound} from "./calc.ts"
+
+    let numberOfReadings = null;
+    let desiredFocalLength = null;
+    let readings = null;
+    let averageFocus = 0;
+
+    function generate() {
+        if (numberOfReadings == null || desiredFocalLength == null) {
+            alert("Please enter appropriate values!");
+            return;
+        }
+        readings = generateReadings(numberOfReadings, desiredFocalLength);
+        averageFocus = chadRound((readings.map(reading => reading.focalLength).reduce((a, b) => a + b)) / readings.length, 2);
+    }
+</script>
+
+<div class="flex flex-col justify-center">
+    <div class="flex flex-col justify-center">
+        <div><input bind:value={desiredFocalLength} class="text-2xl border-2 m-4 w-[80%]" type="number" min="0" id="desiredFocalLength" name="desiredFocalLength" placeholder="Desired Focal Length (cm)"></div>
+        <div class="text-xl"><input bind:value={numberOfReadings} class="w-[80%] text-2xl border-2 m-2" type="number" min="1" max="7" id="numberOfReadings" name="numberOfReadings" placeholder="Number of Readings"></div>
+        <div class="m-4 text-2xl"><button on:click="{generate}" type="button" class="bg-red-500 px-5 text-white rounded">Generate</button></div>
+    </div>
+    <br><br>
+    {#if readings != null}
+        <div class="flex justify-center">
+            <table class="border border-gray-400 lg:text-xl">
+                <tr class="bg-blue-100 border border-gray-400">
+                    <td class="px-7">a</td>
+                    <td class="px-7">b</td>
+                    <td class="px-7">c</td>
+                    <td class="px-7">u</td>
+                    <td class="px-7">v</td>
+                    <td class="px-7">f</td>
+                </tr>
+                {#each readings as reading}
+                    <tr class="border border-gray-300">
+                        <td>{chadRound(reading.aObjectPos, 1)}</td>
+                        <td>{chadRound(reading.cImagePos, 1)}</td>
+                        <td>{chadRound(reading.bLensPos, 1)}</td>
+                        <td>{chadRound(reading.uObjectDistance, 1)}</td>
+                        <td>{chadRound(reading.vImageDistance, 1)}</td>
+                        <td>{chadRound(reading.focalLength, 2)}</td>
+                    </tr>
+                {/each}
+            </table>
+        </div>
+        <br>
+        <div>
+            <div class="justify-center">
+                <ul>
+                    <li>a - Position of object on scale</li>
+                    <li>b - Position of image on scale</li>
+                    <li>c - Position of lens on scale</li>
+                    <li>u - Distance from lens to object</li>
+                    <li>v - Distance from lens to image</li>
+                    <li>f - Focal length</li>
+                </ul>
+            </div>
+        </div>
+        <div><h1>All values are in centimeters unless otherwise stated.</h1></div>
+        <div><h1>Average Focus: {averageFocus}</h1></div>
+    {/if}
+    <br><br>
+</div>
--- src/routes/Main.svelte
+++ src/routes/Main.svelte
@@ -1,21 +1,20 @@
+<script>
+    import Calculator from "./Calculator.svelte";
+</script>
+
 <div class="flex flex-col font-epicfont justify-center text-center pt-4">
     <div class="text-5xl"><h1>Readings Generator</h1></div>
     <div class="text-2xl"><h1>Be smart, not hardworking.</h1></div>
     <br><br>
-    <div class="flex flex-col justify-center">
-        <div><input class="text-2xl border-2 m-4 w-[80%]" type="number" id="desiredFocalLength" name="desiredFocalLength" placeholder="Desired Focal Length (cm)"></div>
-        <div class="text-xl"><input class="w-[80%] text-2xl border-2 m-2" type="number" id="numberOfReadings" name="numberOfReadings" placeholder="Number of Readings"></div>
-        <div class="m-4 text-2xl"><button type="button" class="bg-red-500 px-5 text-white rounded">Generate</button></div>
-    </div>
-    <br><br>
-    <!--TODO-->
+    <Calculator/>
     <br><br>
     <div class="text-3xl lg:pl-40 lg:pr-40 lg:text-left">
         What the heck is this?
     </div>
     <div class="text-xl lg:pl-40 lg:pr-40 lg:text-left">
         It's something that extrapolates results.
-        Input your desired focal length and number of readings required, press the big red button, and instantly get a set of randomized readings that give a concordant reading of your desired focal length.
+        Input your desired focal length and number of readings required, press the big red button, and instantly get a set of randomized readings that give a concordant reading that's very close to your desired focal length.
+        If you dislike your generated readings, press the button again to generate a new set of randomly generated readings.
         If you messed up this experiment this may come of use to you.
         <br>
         <div class="text-s">Source Code: <a href="https://github.com/BoogieMonster1O1/physicsbad" target="_blank" class="text-blue-600">Github</a></div>
