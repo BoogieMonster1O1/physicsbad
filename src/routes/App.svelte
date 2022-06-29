90fec21dbbb2cf802f7329fb9912525e2fbc4e03
SD <harrypotter.starwars.shrish@gmail.com>
2022-06-29 16:34:57 +0530

    Fix missing wrap

--- src/routes/App.svelte
+++ src/routes/App.svelte
@@ -1,5 +1,6 @@
 <script>
     import Router from "svelte-spa-router";
+    import wrap from "svelte-spa-router/wrap"
     import TabView from "./lib/TabView.svelte";
     import Tab from "./lib/Tab.svelte";
     import { faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
@@ -21,29 +22,31 @@
     <title>{`potatoes`}</title>
 </svelte:head>
 
-<div class="flex flex-col font-epicfont justify-center text-center pt-4">
-    <div class="text-5xl"><h1>Readings Generator</h1></div>
-    <div class="text-2xl"><h1>Be smart, not hardworking.</h1></div>
-    <br><br>
-    <Router {routes}/>
-    <br><br>
-    <div class="text-3xl lg:pl-40 lg:pr-40 lg:text-left">
-        What the heck is this?
-    </div>
-    <div class="text-xl lg:pl-40 lg:pr-40 lg:text-left">
-        It's something that extrapolates results.
-        Input your desired focal length and number of readings required, press the big red button, and instantly get a set of randomized readings that give a concordant reading that's very close to your desired focal length.
-        If you dislike your generated readings, press the button again to generate a new set of randomly generated readings.
-        If you messed up this experiment this may come of use to you.
+<main>
+    <div class="flex flex-col font-epicfont justify-center text-center pt-4">
+        <div class="text-5xl"><h1>Readings Generator</h1></div>
+        <div class="text-2xl"><h1>Be smart, not hardworking.</h1></div>
+        <br><br>
+        <Router {routes}/>
+        <br><br>
+        <div class="text-3xl lg:pl-40 lg:pr-40 lg:text-left">
+            What the heck is this?
+        </div>
+        <div class="text-xl lg:pl-40 lg:pr-40 lg:text-left">
+            It's something that extrapolates results.
+            Input your desired focal length and number of readings required, press the big red button, and instantly get a set of randomized readings that give a concordant reading that's very close to your desired focal length.
+            If you dislike your generated readings, press the button again to generate a new set of randomly generated readings.
+            If you messed up this experiment this may come of use to you.
+            <br>
+            <div class="text-s">Source Code: <a href="https://github.com/BoogieMonster1O1/physicsbad" target="_blank" class="text-blue-600">Github</a></div>
+        </div>
         <br>
-        <div class="text-s">Source Code: <a href="https://github.com/BoogieMonster1O1/physicsbad" target="_blank" class="text-blue-600">Github</a></div>
+        <div class="text-xs">
+            Copyright © Shrish Deshpande. MIT Licensed.
+        </div>
+        <br><br>
+        <TabView>
+            <Tab name="Optics UV" icon={faMagnifyingGlass} route=""/>
+        </TabView>
     </div>
-    <br>
-    <div class="text-xs">
-        Copyright © Shrish Deshpande. MIT Licensed.
-    </div>
-    <br><br>
-    <TabView>
-        <Tab name="Optics UV" icon={faMagnifyingGlass} route=""/>
-    </TabView>
-</div>
+</main>
